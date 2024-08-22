796df62e0f640f04416878aced8e1cd4b630a27b
FakeShot <00001125338295sp@al.educacao.sp.gov.br>
2024-08-22 18:24:15 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,89 @@
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background("white");
+    fill("black");
+    textSize(64);
+    textAlign (CENTER, CENTER);
+    
+    let maximo = width;
+    let minimo = 0;
+    // mouseX, 0, width ==> 0, palavra.length
+    let palavra = "Karvalho";
+    let quantidade = map(mouseX, 0, width, 1, palavra.length);
+    //console.log(quantidade);
+    let parcial = palavra.substring(0, quantidade);
+    text(parcial, 200, 200);
+    
+    // if (mouseX < 50) {
+    // let palavra = "C";
+    // text(palavra, 200, 200);
+    // } else if (mouseX < 100) {
+        //let palavra = "Ca";
+        // text(palavra, 200, 200);
+    // } else {
+        //let palavra = "Karvalho";
+        //text(palavra, 200, 200);
+    //}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
